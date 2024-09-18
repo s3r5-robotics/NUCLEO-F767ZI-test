96da4588988e2d75e3a9c0220b21f3fd39c71736
Luka Zamuda <luka.zamuda@sers.si>
2024-09-18 08:35:36 +0200

    Move code to correct comment

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -120,9 +120,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
     HAL_Delay(100);
+    /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
